185a42022fba470a55aba1355921b6b3dfa89242
ESPR3SS0 <>
2023-09-01 16:16:17 -0400

    Updated install script

--- setup.sh
+++ setup.sh
@@ -9,9 +9,11 @@ if [ -d "$HOME/ghidra_10.3.3_PUBLIC" ]; then
     rm -rf ~/ghidra_10.3.3_PUBLIC
 fi
 
-if [ -f "$HOME/ ghidra_10.3.3_PUBLIC_20230829.zip" ]; then
-    rm "$HOME/ ghidra_10.3.3_PUBLIC_20230829.zip"
+if [ -f "$HOME/ghidra_10.3.3_PUBLIC_20230829.zip" ]; then
+    rm "$HOME/ghidra_10.3.3_PUBLIC_20230829.zip"
 fi
+
+CURRENT_DIR="$PWD"
 
 cd ~ && wget https://github.com/NationalSecurityAgency/ghidra/releases/download/Ghidra_10.3.3_build/ghidra_10.3.3_PUBLIC_20230829.zip
 cd ~ && unzip ghidra_10.3.3_PUBLIC_20230829.zip
@@ -19,4 +21,11 @@ cd ~ && unzip ghidra_10.3.3_PUBLIC_20230829.zip
 echo "Installed ghidra"
 
 
+if  [ ! -d "$HOME/ghidra_scripts" ]; then
+    mkdir $HOME/ghidra_scripts
+fi
+
+if [ ! -f "$HOME/ghidra_10.3.3_PUBLIC_20230829.zip" ]; then
+    cp $CURRENT_DIR/List_Function_and_Entry.py $HOME/ghidra_scripts/
+fi
 
